51b9780741cf3bc4796edb0122c979b23cd1abd6
WENDEL FERREIRA DA SILVA <WESILVA@trf3.jus.br>
2019-09-17 14:16:42 -0300

    revisando branch master

--- codeflix-app/src/app/app.container.ts
+++
@@ -1,9 +0,0 @@
-import {Injector} from "@angular/core";
-
-let localInjector:Injector;
-export const appContainer = (injector?:Injector):Injector =>{
-    if(injector){
-        localInjector = injector;
-    }
-    return localInjector;
-};
--- codeflix-app/src/models/jwt-credentials.ts
+++
@@ -1,4 +0,0 @@
-export interface JwtCredentials{
-    email;
-    password;
-}
